e957ac973d4b812e8021ffb90ba6640f5194fecc
Combos <combos93@mail.ru>
2018-11-07 21:11:33 +0600

    add sha2 encrypt

--- crypto.rb
+++ crypto.rb
@@ -15,18 +15,22 @@ talk = STDIN.gets.chomp
 puts "Каким способом зашифровать:"
 puts "1. MD5"
 puts "2. SHA1"
+puts "3. SHA2"
 crypt = STDIN.gets.chomp
-  while (crypt != "1" and crypt != "2")
-    puts "Введите либо 1, либо 2"
-    puts "1. MD5"
-    puts "2. SHA1"
-    crypt = STDIN.gets.chomp
-  end
+
+while (crypt != "1" and crypt != "2" and crypt != "3")
+  puts "Введите либо 1, либо 2, либо 3"
+  puts "1. MD5"
+  puts "2. SHA1"
+  puts "3. SHA2"
+  crypt = STDIN.gets.chomp
+end
 
 case crypt
 when "1"
   puts Digest::MD5.hexdigest("#{talk}")
 when "2"
   puts Digest::SHA1.hexdigest("#{talk}")
+when "3"
+  puts Digest::SHA2.hexdigest("#{talk}")
 end
-

7f02c08767abf361ac9077b9f9218122e0a70804
Combos93 <combos93@mail.ru>
2018-12-19 19:07:57 +0600

    Clear refactor by RM

--- crypto.rb
+++ crypto.rb
@@ -1,36 +1,27 @@
 require 'digest'
 
-if (Gem.win_platform?)
-  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
-  Encoding.default_internal = __ENCODING__
-
-  [STDIN, STDOUT].each do |io|
-    io.set_encoding(Encoding.default_external, Encoding.default_internal)
-  end
-end # Гем на русский язык!
-
-puts "Введите фразу"
+puts 'Введите фразу'
 talk = STDIN.gets.chomp
 
-puts "Каким способом зашифровать:"
-puts "1. MD5"
-puts "2. SHA1"
-puts "3. SHA2"
+puts 'Каким способом зашифровать:'
+puts '1. MD5'
+puts '2. SHA1'
+puts '3. SHA2'
 crypt = STDIN.gets.chomp
 
-while (crypt != "1" and crypt != "2" and crypt != "3")
-  puts "Введите либо 1, либо 2, либо 3"
-  puts "1. MD5"
-  puts "2. SHA1"
-  puts "3. SHA2"
+while crypt != '1' && crypt != '2' && crypt != '3'
+  puts 'Введите либо 1, либо 2, либо 3'
+  puts '1. MD5'
+  puts '2. SHA1'
+  puts '3. SHA2'
   crypt = STDIN.gets.chomp
 end
 
 case crypt
-when "1"
-  puts Digest::MD5.hexdigest("#{talk}")
-when "2"
-  puts Digest::SHA1.hexdigest("#{talk}")
-when "3"
-  puts Digest::SHA2.hexdigest("#{talk}")
+when '1'
+  puts Digest::MD5.hexdigest(talk.to_s)
+when '2'
+  puts Digest::SHA1.hexdigest(talk.to_s)
+else
+  puts Digest::SHA2.hexdigest(talk.to_s)
 end
